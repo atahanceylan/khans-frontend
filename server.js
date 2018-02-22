a077df8e85da8a1c4cd4744c535c9efe0a4c3130
Atahan <atahanceylan@gmail.com>
2018-02-22 23:08:10 +0300

    update server.js

--- server.js
+++ server.js
@@ -18,6 +18,5 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/src/index.html'));
 });
 
-
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 5000);
