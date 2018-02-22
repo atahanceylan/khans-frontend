fd13996e8f34dfdb4de861f30e26790b5d4edc01
Atahan <atahanceylan@gmail.com>
2018-02-22 22:52:11 +0300

    update server.js

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const app = express();
 //app.use(express.static(__dirname + '/dist'));
 var path = require('path');
 
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/src/index.html'));
+});
 
 
 // Start the app by listening on the default Heroku port
