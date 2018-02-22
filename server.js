401be741e35f41c13e6fe86fc37294db1ca28d76
Atahan <atahanceylan@gmail.com>
2018-02-22 22:39:11 +0300

    update server.js

--- server.js
+++ server.js
@@ -4,19 +4,12 @@ const app = express();
 
 // Serve only the static files form the dist directory
 //app.use(express.static(__dirname + '/dist'));
-var options = {
-  dotfiles: 'ignore',
-  etag: false,
-  extensions: ['htm', 'html'],
-  index: false,
-  maxAge: '1d',
-  redirect: false,
-  setHeaders: function (res, path, stat) {
-    res.set('x-timestamp', Date.now())
-  }
-}
+var path = require('path');
 
-app.use(express.static('public', options))
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + 'src/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
